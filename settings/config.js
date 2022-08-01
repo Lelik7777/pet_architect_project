fc7a2fab87ee3756e6c7fee4f6eb05da24345e37
Lelik Jan <21alex7777@gmail.com>
2022-08-01 11:26:27 +0300

    feat: add config.js and source-map.jse

---
+++ settings/config.js
@@ -0,0 +1,10 @@
+module.exports={
+    output:'src',
+    replace:false,
+    clean:false,
+    itrStart:'{{',
+    itrEnd:'}}',
+    itrFileNameStart:'[',
+    itrFileNameEnd:']',
+    templateExt:'.txt',
+}
---
+++ settings/source_map/source-map.js
@@ -0,0 +1,12 @@
+module.exports = {
+    aliases: {
+        'react-component': 'rc',
+        store: 's',
+        index: 'i',
+    },
+    map: {
+        'components': {
+            component: 'component'
+        }
+    }
+}
